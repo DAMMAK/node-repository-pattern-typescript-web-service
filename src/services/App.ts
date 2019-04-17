9d6e06bdbc33e693d6ecad1bbac669d5c457c3a8
DAMMAK <adekoyafelix@gmail.com>
2019-04-17 18:14:14 +0100

    Adding Error Handler

--- src/services/App.ts
+++ src/services/App.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "../middlewares/cors";
 import mainRoute from "../routes/main";
+import errorHandler from "../middlewares/err";
+import error from "../middlewares/404";
+
 class App {
   public app: express.Application;
   constructor() {
@@ -11,6 +14,8 @@ class App {
   public injectMiddleWare(): void {
     this.app.use(cors);
     this.app.use(mainRoute);
+    this.app.use(errorHandler);
+    this.app.use(error);
   }
 }
 export default new App().app;
